bc63503cd5d45bc491c7f5c6fbe0bc8983c1ef4b
Jacob Ayinsakia <jacob.ayinsakia@hubtel.com>
2025-05-29 18:26:54 +0000

    Updated classes and instances for Hostel App

--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 import pandas
-from pandas.plotting import hist_frame
 
-df = pandas.read_csv("hostels.csv")
+df = pandas.read_csv("hotels.csv")
 
 
-class Hostel:
-    def __init__(self, id):
-        pass
+class Hotel:
+    def __init__(self, hotel_id):
+        self.hotel_id = hotel_id
 
     def book(self):
-        pass
+        df.loc[["id"] == self.hotel_id, "available"] = "no"
+        df.to_csv("hotels.csv", index=False)
 
     def available(self):
-        pass
+        availability = df.loc[df["id"] == self.hotel_id, "available"].squeeze()
+        if availability == "yes":
+            return True
+        else:
+            return False
 
 class ReservationTicket:
-    def __init__(self, customer_name, hostel_object):
+    def __init__(self, customer_name, hotel_object):
         pass
 
     def generate(self):
-        content = f"Name of the customer hostel"
+        content = f"Name of the customer hotel"
         return  content
 
 
 
 print(df)
-id = input("Enter the id of the hostel: ")
-hostel_id = Hostel(id)
+hotel_ID = input("Enter the id of the hostel: ")
+hotel = Hotel(hotel_ID)
 
-if hostel.available():
-    hostel.book()
+if hotel.available():
+    hotel.book()
     name = input("Enter your name: ")
-    reservation_ticket = ReservationTicket(name, hostel)
+    reservation_ticket = ReservationTicket(name, hotel)
     print(reservation_ticket.generate())
 else:
     print("Hotel is not free.")
