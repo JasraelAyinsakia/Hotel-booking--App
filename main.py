c08a8c915839c24ec7ef63690d38b39ee2b20f9f
Jacob Ayinsakia <jacob.ayinsakia@hubtel.com>
2025-05-30 23:15:21 +0000

    Added class Reservation

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import pandas
 
-df = pandas.read_csv("hotels.csv")
+df = pandas.read_csv("hotels.csv", dtype={"id": str})
 
 
 class Hotel:
     def __init__(self, hotel_id):
         self.hotel_id = hotel_id
+        self.name = df.loc[df["id"] == self.hotel_id, "name"].squeeze
 
     def book(self):
-        df.loc[["id"] == self.hotel_id, "available"] = "no"
+        df.loc[df["id"] == self.hotel_id, "available"] = "no"
         df.to_csv("hotels.csv", index=False)
 
     def available(self):
@@ -20,13 +21,17 @@ class Hotel:
 
 class ReservationTicket:
     def __init__(self, customer_name, hotel_object):
-        pass
+        self.customer_name = customer_name
+        self.hotel = hotel_object
 
     def generate(self):
-        content = f"Name of the customer hotel"
-        return  content
-
-
+        content = f"""
+        Thank you for your reservation!
+        Here are your booking data:
+        Name: {self.customer_name} 
+        Hotel name: {self.hotel.name}
+        """
+        return content
 
 print(df)
 hotel_ID = input("Enter the id of the hostel: ")
